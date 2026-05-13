8bec33df3073c5a848c58c73123244b7f51cfe62
RAZAFINDRAVONONA Angeli Iriantsoa <iriantsoaangeli@gmail.com>
2026-05-13 10:43:17 +0300

    Model conge

---
+++ app/Models/Conge.php
@@ -0,0 +1,28 @@
+<?
+
+namespace App\Models;
+use CodeIgniter\Model;
+
+class Conge extends Model {
+
+    protected $table = 'conges';
+    protected $primaryKey = 'id';
+
+    protected $returnType = 'array';
+    protected $useSoftDeletes = false;
+
+    protected $allowedFields = [
+        'id',
+        'employe_id',
+        'type_conge_id',
+        'status_id',
+        'nb_jours',
+        'date_debut',
+        'date_fin',
+        'commentaire_rh',
+        'motif' ,
+        'traite_par',
+        'created_at',
+        ];
+    
+}
---
+++ app/Models/StatusConge.php
@@ -0,0 +1,20 @@
+<?
+
+namespace App\Models;
+use CodeIgniter\Model;
+
+class StatusConge extends Model
+{
+    protected $table = 'status_conges';
+    protected $primaryKey = 'id';
+
+    protected $returnType = 'array';
+    protected $useSoftDeletes = false;
+
+    protected $allowedFields = [
+        'id',
+        'nom',
+    ];
+
+}
+
